c38d61df68a16b32671675c3152dcf22940a5e81
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-09-02 19:11:16 -0700

    changed heading

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Clark's Awesome Screen Printing & Other Stuff</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">Clark's Awesome <br/>Screen Printing</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">If you want your custom shirts done right the first time come to Clark</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
